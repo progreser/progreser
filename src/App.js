1576cae99fbecf7c492003d2c54b7c47f823281b
jungjh123456 <jungjh123456@naver.com>
2021-01-11 20:20:56 +0900

    config: Routin

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Routin from './components/Routin';
-
+import Routin from './components/Routine';
+import Modify from './components/Modify';
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/modify" component={Modify} />
         <Route path="/signup" component={Signup} />
         <Route path="/" exact component={Routin} />
       </Switch>
--- src/index.js
+++ src/index.js
@@ -5,17 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
-// // import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-
-// const store = createStore();
-
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals();

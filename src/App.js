6e8e53fb457585f7598d5b4921c7ac7158590124
jungjh123456 <jungjh123456@naver.com>
2021-01-19 14:47:03 +0900

    feat: NewRoutineContainer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Routine from './components/Routine';
-import Modify from './components/Modify';
+import NewRoutine from './components/NewRoutine';
 
 import createSagaMiddleware from 'redux-saga';
 import { applyMiddleware, createStore } from 'redux';
@@ -32,7 +32,7 @@ function App() {
         <ConnectedRouter history={history}>
           <Switch>
             <Route path="/login" component={LoginContainer} />
-            <Route path="/modify" component={Modify} />
+            <Route path="/newroutine" component={NewRoutine} />
             <Route path="/signup" component={SignContainer} />
             <Route path="/" exact component={Routine} />
           </Switch>

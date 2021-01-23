f347755106bb955ee6b4bc84ce96bab0da3718d7
jungjh123456 <jungjh123456@naver.com>
2021-01-23 18:07:34 +0900

    modal

--- src/ModalPortal/MyModal/MyModal.jsx
+++ src/ModalPortal/MyModal/MyModal.jsx
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import Modalloaders from './components/Modalloaders';
 import './MyModal.scss';
 
-const MyModal = () => {
-  const [loading, setLoading] = useState(false);
-  const [state, setState] = useState(false);
+const MyModal = props => {
+  console.log(props);
+
+  const visible = () => {
+    props.onClick();
+  };
   return (
     <div className="modal">
       <div>
         <Modalloaders />
-        <button>닫기</button>
+        <span>아이디가 중복 되었습니다.</span>
+        <button onClick={visible}>닫기</button>
       </div>
     </div>
   );
